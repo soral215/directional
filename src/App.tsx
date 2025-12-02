133f2cf7e70fa5a73c9d7776d87746d8dab0c39b
soral215 <sora901215@gmail.com>
2025-12-02 15:42:32 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center py-8">Directional</h1>
+    </div>
+  )
+}
+
+export default App
